bf235df4ed6c44a3b5a787220e4e1b26ddce6f31
Devon Stewart <dstewart@twilio.com>
2018-03-17 15:07:13 +0900

    Extending BigObject test to represent more facets

--- src/test/scala/swagger/protocols/BigObject.scala
+++ src/test/scala/swagger/protocols/BigObject.scala
@@ -25,6 +25,7 @@ class BigObjectSpec extends FunSuite with Matchers {
     |      v1:
     |        type: integer
     |        format: int32
+    |        readOnly: true
     |      v2:
     |        type: integer
     |        format: int32
@@ -110,8 +111,8 @@ class BigObjectSpec extends FunSuite with Matchers {
     |        type: integer
     |        format: int32
     |      v30:
-    |        type: integer
-    |        format: int32
+    |        type: string
+    |        x-scala-empty-is-null: true
     |""".stripMargin)
 
   test("Big objects can be generated") {
@@ -119,12 +120,12 @@ class BigObjectSpec extends FunSuite with Matchers {
     val ClassDefinition(_, _, cls, cmp) :: _ = definitions
 
     val definition = q"""
-      case class BigObject(v1: Option[Int] = None, v2: Option[Int] = None, v3: Option[Int] = None, v4: Option[Int] = None, v5: Option[Int] = None, v6: Option[Int] = None, v7: Option[Int] = None, v8: Option[Int] = None, v9: Option[Int] = None, v10: Option[Int] = None, v11: Option[Int] = None, v12: Option[Int] = None, v13: Option[Int] = None, v14: Option[Int] = None, v15: Option[Int] = None, v16: Option[Int] = None, v17: Option[Int] = None, v18: Option[Int] = None, v19: Option[Int] = None, v20: Option[Int] = None, v21: Option[Int] = None, v22: Option[Int] = None, v23: Option[Int] = None, v24: Option[Int] = None, v25: Option[Int] = None, v26: Option[Int] = None, v27: Option[Int] = None, v28: Option[Int] = None, v29: Option[Int] = None, v30: Option[Int] = None)
+      case class BigObject(v1: Option[Int] = None, v2: Option[Int] = None, v3: Option[Int] = None, v4: Option[Int] = None, v5: Option[Int] = None, v6: Option[Int] = None, v7: Option[Int] = None, v8: Option[Int] = None, v9: Option[Int] = None, v10: Option[Int] = None, v11: Option[Int] = None, v12: Option[Int] = None, v13: Option[Int] = None, v14: Option[Int] = None, v15: Option[Int] = None, v16: Option[Int] = None, v17: Option[Int] = None, v18: Option[Int] = None, v19: Option[Int] = None, v20: Option[Int] = None, v21: Option[Int] = None, v22: Option[Int] = None, v23: Option[Int] = None, v24: Option[Int] = None, v25: Option[Int] = None, v26: Option[Int] = None, v27: Option[Int] = None, v28: Option[Int] = None, v29: Option[Int] = None, v30: Option[String] = None)
     """
     val companion = q"""
       object BigObject {
         implicit val encodeBigObject = {
-          val readOnlyKeys = Set[String]()
+          val readOnlyKeys = Set[String]("v1")
           new ObjectEncoder[BigObject] { final def encodeObject(o: BigObject): JsonObject = JsonObject.fromIterable(Vector(("v1", o.v1.asJson), ("v2", o.v2.asJson), ("v3", o.v3.asJson), ("v4", o.v4.asJson), ("v5", o.v5.asJson), ("v6", o.v6.asJson), ("v7", o.v7.asJson), ("v8", o.v8.asJson), ("v9", o.v9.asJson), ("v10", o.v10.asJson), ("v11", o.v11.asJson), ("v12", o.v12.asJson), ("v13", o.v13.asJson), ("v14", o.v14.asJson), ("v15", o.v15.asJson), ("v16", o.v16.asJson), ("v17", o.v17.asJson), ("v18", o.v18.asJson), ("v19", o.v19.asJson), ("v20", o.v20.asJson), ("v21", o.v21.asJson), ("v22", o.v22.asJson), ("v23", o.v23.asJson), ("v24", o.v24.asJson), ("v25", o.v25.asJson), ("v26", o.v26.asJson), ("v27", o.v27.asJson), ("v28", o.v28.asJson), ("v29", o.v29.asJson), ("v30", o.v30.asJson))) }.mapJsonObject(_.filterKeys(key => !(readOnlyKeys contains key)))
         }
         implicit val decodeBigObject = new Decoder[BigObject] {
@@ -158,7 +159,7 @@ class BigObjectSpec extends FunSuite with Matchers {
             v26 <- c.downField("v27").as[Option[Int]]
             v27 <- c.downField("v28").as[Option[Int]]
             v28 <- c.downField("v29").as[Option[Int]]
-            v29 <- c.downField("v30").as[Option[Int]]
+            v29 <- c.downField("v30").withFocus(j => j.asString.fold(j)(s => if (s.isEmpty) Json.Null else j)).as[Option[String]]
           } yield BigObject(v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29)
         }
       }
